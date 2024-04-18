2cab9003c3bb335304bbbd5d5e5e4ff520cff04b
std3 <67806187+standard3@users.noreply.github.com>
2024-04-18 17:09:11 +0200

    feat: created encryption module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod encryption;
 mod payloadplacement;
 
 fn main() {
     #[cfg(feature = "payloadplacement")]
     payloadplacement::entry();
+
+    #[cfg(feature = "encryption")]
+    encryption::entry();
 }
